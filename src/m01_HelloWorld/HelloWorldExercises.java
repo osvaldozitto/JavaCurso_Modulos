1f9d38813a104762031ff68473f907b3aa5613c9
lanbide13 <lanbide13@arriagainfo.local>
2025-11-11 20:19:37 +0100

    primera clase

--- src/m01_HelloWorld/HelloWorldExercises.java
+++ src/m01_HelloWorld/HelloWorldExercises.java
@@ -4,18 +4,24 @@ public class HelloWorldExercises {
 
     public static void main(String[] args) {
 
+        System.out.println("Hola mundo");
+
         // 1. Imprime un mensaje que diga tu nombre en lugar de "¡Hola Mundo!".
+System.out.println ("Hola Osvaldo");
 
         // 2. Imprime dos líneas: "Hola" y luego "Mundo" con un solo println.
-
+System.out.println("Hola");
+System.out.println("Mundo");
         // 3. Añade un comentario sobre lo que hace cada línea del programa.
-
+System.out.println("");
         // 4. Crea un comentario en varias líneas.
-
+System.out.println("");
         // 5. Imprime tu edad, tu color favorito y tu ciudad.
-
+System.out.println("37");
+System.out.println("Rojo");
+System.out.println("Vitoria");
         // 6. Explora los diferentes System.XXX.println(); más allá de "out".
-
+        System.err.println("Hola como estas");
         // 7. Utiliza varios println para imprimir una frase.
 
         // 8. Imprime un diseño ASCII (por ejemplo, una cara feliz usando símbolos).
